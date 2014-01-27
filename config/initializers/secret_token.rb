cab8c70851bc41c13b91a1b688872189f128c757
Mark Dunne <markdunne02@gmail.com>
2014-01-27 22:47:29 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CharityHost::Application.config.secret_key_base = '2324f0a53e10120f5e9bfaa6bdc61486525abbdd1a929602c9061400b0a9d646cdab0a0bfa64066140a7398aba6a25c4db18f193c83181afbbc15041f7687e84'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CharityHost::Application.config.session_store :cookie_store, key: '_charity_host_session'

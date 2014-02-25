780c48541ad1e667ddcef27e027d86269433802d
Mark Dunne <mark.dunne02@gmail.com>
2014-02-25 00:18:40 +0000

    added seedds

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,19 +8,4 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
   has_many :newsletter_subscriptions
-
-
-  after_create :accept_admin_offers
-
-  private
-
-  def accept_admin_offers
-  	CharityInviteOffers.where(email: self.email).each do |offer|
-  		if !offer.accepted
-  			CharitiesUsers.create(user_id: self.id, charity_id: offer.charity_id)
-  			offer.accepted = true
-  			offer.save
-  		end
-  	end
-  end
 end

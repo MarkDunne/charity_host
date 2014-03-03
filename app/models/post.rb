81dc8a1cce9127dbab6121932add225bbbc79a4b
Mark Dunne <mark.dunne02@gmail.com>
2014-03-03 23:32:01 +0000

    fixed tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ActiveRecord::Base
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
   has_many :comments
-  has_many :post_tags
-  has_many :post_photos
+  has_many :tags, class_name: "PostTag"
+  has_many :photos, class_name: "PostPhoto"
   has_one :animal_detail
 
   def has_tag (tag)

efb21f8f4daba6e302a38ae739f4c870ed7d718a
Mark Dunne <mark.dunne02@gmail.com>
2014-03-20 12:48:35 +0000

    default post enable

--- db/migrate/20140203205132_create_charity_settings.rb
+++ db/migrate/20140203205132_create_charity_settings.rb
@@ -6,7 +6,7 @@ class CreateCharitySettings < ActiveRecord::Migration
       t.string :text_color
       t.string :header_image
       t.boolean :enable_paypal_donations
-      t.boolean :enable_post_comments
+      t.boolean :enable_post_comments, default: true
       t.string :paypal_email
       t.text :about, default: "Update 'About Your Charity' in the admin view"
       t.timestamps
